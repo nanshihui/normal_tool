0e6ee64bb7e1a599219ab20e377555e2fc819f64
nanshihui <nanshihui@qq.com>
2016-05-12 10:29:20 +0800

    fix bug

--- countipnum/daily_record_read.py
+++ countipnum/daily_record_read.py
@@ -2,12 +2,13 @@
 #coding:utf-8
 
 
-
+import time
 
 def readfile(path,callback=None,needWrite=False,writeFilename='default.log',finallycallback=None,keywords=None):
 	f = open (path)
 	params=None
 	filePoint=None
+
 	if needWrite:
 		filePoint=file(writeFilename,'w')
 	while 1:
@@ -17,11 +18,12 @@ def readfile(path,callback=None,needWrite=False,writeFilename='default.log',fina
 		if callback is None:
 			pass
 		else:
-
+			
 			params=callback(line,filePoint,params,keywords)
+
 	if finallycallback is not None:
 
-		finallycallback(filePoint,params)
+		finallycallback(filePoint,params,keywords)
 	if needWrite:
 		filePoint.close()
 
@@ -30,7 +32,12 @@ def deal_file(line,writeplace=None,params=None,keywords=None):
 	linearray = line.split()
 	# if  '10/May/2016:23' in linearray[3]  :
 	# if  '14.117.49.148' in linearray[0] and linearray[8]=='200' and '.' not in linearray[6]:
-	if  '10/May/2016:23' in linearray[3] and linearray[8]=='200' and 'evaluate'  in linearray[6]:
+	# if  '04/May/2016:16' in linearray[3] and linearray[8]=='200' and 'evaluate'  in linearray[6]:
+	# if  '04/May/2016:16' in linearray[3] :
+	if keywords is None:
+		return params
+
+	if  keywords[0] in linearray[3] and linearray[8]==keywords[1] and keywords[2]  in linearray[6]:
 		if writeplace is not None:
 
 			writeplace.write(line)
@@ -38,7 +45,7 @@ def deal_file(line,writeplace=None,params=None,keywords=None):
 def keyword_file(line,writeplace=None,params=None,keywords=None):
 	linearray = line.split()
 
-	if  keywords in linearray[0] and linearray[8]=='200' and '.' not in linearray[6]:
+	if  keywords[4] in linearray[0] and linearray[8]==keywords[1] and '.' not in linearray[6] and  keywords[1][2:] in linearray[3]:
 		if writeplace is not None:
 
 			writeplace.write(line)
@@ -55,17 +62,24 @@ def countIPnumber(line,writeplace=None,dic=None,keywords=None):
 		else:
 			dic[linearray[0]]=dic[linearray[0]]+1
 	return dic
-def write_dic(filePoint,params):
+def write_dic(filePoint,params,keywords=None):
+
 	for i in params.keys():
 		filePoint.write(str(params[i])+'  '+str(i)+'\n')
-		readfile(path='access.log',callback=keyword_file,needWrite=True,writeFilename=str(i)+'.log',keywords=str(i))
+
+		keywords.append(str(i))
+		localtime=str(time.strftime("%Y-%m-%d-%X", time.localtime()))
+
+		readfile(path=keywords[3],callback=keyword_file,needWrite=True,writeFilename=str(i)+'.'+localtime+'.log',keywords=keywords)
 	
 if __name__ == "__main__":
 	keywords='evaluate'
-	readfile(path='access.log',callback=deal_file,needWrite=True,writeFilename=keywords+'.log')
+	keyarray=['11/May/2016:00:0','200','evaluate','access.log']
+	localtime=str(time.strftime("%Y-%m-%d-%X", time.localtime()))
+	readfile(path=keyarray[3],callback=deal_file,needWrite=True,writeFilename=keywords+'.'+localtime+'.log',keywords=keyarray)
 
 
 
-	readfile(path=keywords+'.log',callback=countIPnumber,needWrite=True,writeFilename='IPcount.log',finallycallback=write_dic)
+	readfile(path=keywords+'.'+localtime+'.log',callback=countIPnumber,needWrite=True,writeFilename='IPcount'+localtime+'.log',finallycallback=write_dic,keywords=keyarray)
 
 
